842beb7d11f52853592aa5d59e82f1b220cec002
tenkus47 <tenkus47@gmail.com>
2024-09-07 22:13:09 +0530

    update .env AWS BUCKET

--- v1/libs/upload_file_to_s3.py
+++ v1/libs/upload_file_to_s3.py
@@ -10,7 +10,7 @@ load_dotenv(override=True)
 # AWS S3 Configuration
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_KEY")
-AWS_S3_BUCKET_NAME = os.getenv("BUCKET_NAME")
+AWS_S3_BUCKET_NAME = os.getenv("AWS_BUCKET_NAME")
 AWS_S3_REGION_NAME = os.getenv("AWS_REGION")
 
 async def upload_file_to_s3(file: Any, content_type: str, filename: str) -> str:
