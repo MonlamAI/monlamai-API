ddcb16a3ab15956f2914adc9a396e2c6efdcfe5e
tenkus47 <tenkus47@gmail.com>
2024-09-23 17:07:57 +0530

    get the first ip while fetching city

--- v1/utils/utils.py
+++ v1/utils/utils.py
@@ -1,6 +1,12 @@
 from v1.auth.token_verify import verify
 import requests
 
+def get_first_ip(ip_string):
+    # Split the string by commas and strip any whitespace
+    ip_list = [ip.strip() for ip in ip_string.split(',')]
+    # Return the first IP if the list is not empty, otherwise return an empty string
+    return ip_list[0] if ip_list else ''
+
 def get_client_metadata(client_request):
     client_ip = client_request.headers.get('Client-IP', 'Unknown')
     origin_ip = client_request.client.host
@@ -8,7 +14,8 @@ def get_client_metadata(client_request):
     referer = client_request.headers.get('Referer', 'Unknown')
     source_app = origin if origin != 'Unknown' else referer
     client_ip = client_ip if client_ip else origin_ip
-    city, country = get_geolocation(client_ip)
+
+    city, country = get_geolocation(get_first_ip(client_ip))
     return client_ip, source_app,city, country
 
 def get_geolocation(ip_address):
